66e16b7e4817e53d2da66285276a0a90d3cb6094
dfonsat2021 <dimitri.fonsat2021@campus-eni.fr>
2023-06-27 17:02:23 +0200

    update profile form  et controller manage Profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Entity\User;
+use App\Form\ProfileManagerType;
 use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,10 +18,20 @@ class ProfileController extends AbstractController
 {
 
     #[Route('/profile', name: 'app_profile')]
-    public function manageProfile(ParticipantRepository $participantRepository): Response
+    public function manageProfile(EntityManagerInterface $entityManager,
+                                  ParticipantRepository $participantRepository,
+                                    Request $request
+    ): Response
     {
+        $participant = new Participant();
+        $user = new User();
+        $profileForm = $this->createForm(ProfileManagerType::class, $participant);
+        $profileForm->handleRequest($request);
 //        $participant = $participantRepository->find($id);
 
-        return $this->render('profile/manageProfile.html.twig');
+        return $this->render('profile/manageProfile.html.twig', [
+            "profileForm" => $profileForm->createView(),
+            "participant" => $participant,
+        ]);
     }
 }
--- src/Form/ProfileManagerType.php
+++ src/Form/ProfileManagerType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Participant;
+use App\Entity\Site;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +17,38 @@ class ProfileManagerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('prenom')
-            ->add('nom')
-            ->add('telephone')
+            ->add('compte', EntityType::class, [
+                'class' => User::class,
+                'label' => 'Pseudo',
+                'choice_label' => 'Pseudo',
+                'mapped' => false
+            ])
+            ->add('prenom', null, [
+                'label' => 'Prénom'
+            ])
+            ->add('nom', null, [
+                'label' => 'nom'
+            ])
+            ->add('telephone', null, [
+                'label' => 'téléphone'
+            ])
+            ->add('email', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'email',
+                'mapped' => false,
+            ])
+            ->add('motDePasse', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'mapped' => false,
+            ])
+            ->add('motDePasse2', PasswordType::class, [
+                'label' => 'Confirmation',
+                'mapped' => false,
+            ])
+            ->add('site', EntityType::class, [
+                'class' => Site::class,
+                'choice_label' => 'nom'
+            ])
         ;
     }
 
