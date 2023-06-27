c042dc91b6d8ef9618543202f1e255bf757a21ee
dfonsat2021 <dimitri.fonsat2021@campus-eni.fr>
2023-06-27 09:12:52 +0200

    Main controller + route vers l'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,18 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): Response
-    {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
-    }
 
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
-        return $this->render('main/home.html.twig');
+        return $this->render('security/login.html.twig');
     }
 
 }
