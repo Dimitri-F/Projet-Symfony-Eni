840c61acb77ef9ee1de710aa48bd78c135251d65
Seb <sebc118@gmail.com>
2023-06-27 09:38:15 +0200

    update MainController.php login

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
 
-    #[Route('/', name: 'app_home')]
-    public function home(): Response
+    #[Route('/', name: 'app_login')]
+    public function home(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/login.html.twig');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
 }
