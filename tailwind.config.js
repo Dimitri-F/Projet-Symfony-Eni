b9697d2dc6a76268994539dec5810d72d4547bc9
Seb <sebc118@gmail.com>
2023-07-04 17:00:32 +0200

    gestion user admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./assets/**/*.js",
+    "./assets/*.js",
     "./templates/**/*.html.twig",
-    './node_modules/tw-elements/dist/js/**/*.js',
+    "./templates/*.html.twig",
+    // './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"),require("tw-elements/dist/plugin.cjs")],
+  plugins: [
+      require("daisyui"),
+    // require("tw-elements/dist/plugin.cjs")
+  ],
   darkMode: "class",
   daisyui: {
     themes: ["light", "dark", "cupcake"],
