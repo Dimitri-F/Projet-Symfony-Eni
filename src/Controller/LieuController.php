47875a828dcdd98e40e72ec963c0473d3f17469e
rlagadec2023 <lagadecrobin@gmail.com>
2023-07-04 16:55:52 +0200

    gerer les lieux (admin) manque le filtre de recherche

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\FilterCityType;
 use App\Form\LieuType;
 use App\Entity\Lieu;
+use App\Repository\LieuRepository;
+use App\Repository\VilleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Config\Definition\Exception\Exception;
@@ -31,6 +34,75 @@ class LieuController extends AbstractController
         }
         return $this->render('lieu/create.html.twig',[
             "lieuForm" => $lieuForm->createView(),
+        ]);
+    }
+    #[Route('/lieu/manage', 'app_manage_lieu')]
+    public function manage(VilleRepository $villeRepository, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $villes = $villeRepository->findAll();
+        $filterForm = $this->createForm(FilterCityType::class);
+        $filterForm->handleRequest($request);
+
+        $filteredCities = [];
+
+        if ($filterForm->isSubmitted() && $filterForm->isValid()) {
+            $searchTerm = $filterForm->getData()['nom'];
+            $filteredCities = $villeRepository->findFilterCity($searchTerm);
+        }
+
+        return $this->render('lieu/managePlace.html.twig', [
+            "villes" => $villes,
+            "filterCity" => $filterForm->createView(),
+            "filteredCities" => $filteredCities,
+        ]);
+    }
+
+
+
+    #[Route('/lieu/update/{id}','app_update_ville')]
+    public function update($id,VilleRepository $villeRepository): Response
+    {
+        $ville = $villeRepository->find($id);
+        $lieux = [];
+        foreach ($ville->getLieux() as $lieu) {
+            $lieux[] = $lieu->getNom();
+        }
+        return $this->render('lieu/update.html.twig',[
+            "ville" => $ville,
+            "lieux" => $lieux
+        ]);
+    }
+
+    #[Route('/lieu/delete/{id}','app_remove_ville')]
+    public function delete($id,VilleRepository $villeRepository,EntityManagerInterface $entityManager) : Response
+    {
+        try {
+            $ville = $villeRepository->find($id);
+            $lieux = $ville->getLieux();
+            foreach ($lieux as $lieu){
+                if ($lieu->getSorties()->count() > 0) {
+                    $this->addFlash('danger','Impossible de supprimer la ville car des lieux sont liés à des sorties.');
+                    return $this->redirectToRoute('app_manage_lieu');
+                }
+                $ville->removeLieux($lieu);
+                $entityManager->remove($lieu);
+            }
+            $entityManager->remove($ville);
+            $entityManager->flush();
+            $this->addFlash('success','Le ville a bien été supprimée dans la base de données');
+            return $this->redirectToRoute('app_manage_lieu');
+        }catch (Exception $exception){
+            $this->addFlash('danger','Erreur lors de la suppression de la ville');
+        }
+        return $this->render('lieu/delete.html.twig');
+    }
+
+    public function updatePlace($id,LieuRepository $lieuRepository) : Response
+    {
+        $lieu = $lieuRepository->find($id);
+
+        return $this->render('lieu/updatePlace.html.twig',[
+            "lieu" => $lieu
         ]);
     }
 }
--- src/Form/FilterCityType.php
+++ src/Form/FilterCityType.php
@@ -4,28 +4,22 @@ namespace App\Form;
 
 use App\Entity\Ville;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CityType extends AbstractType
+class FilterCityType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', null,[
-                'label' => 'Nom :',
-                'required' => false,
-                'attr' => [
-                    'pattern' => false,
+            ->add('nom',null,[
+                "attr" => [
+                    "pattern" => false
                 ]
             ])
-            ->add('codePostal', TextType::class,[
-                'label' => 'Code Postal :',
-                'required' => false,
-                'attr' => [
-                    'pattern' => false,
-                ],
+            ->add('search', SubmitType::class,[
+                "label" => "Rechercher",
             ])
         ;
     }
